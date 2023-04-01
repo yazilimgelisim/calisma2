297dd963f07ba3623ee909190238d3c0a252cc06
yazilimgelisim <motocelik23@gmail.com>
2023-04-01 14:36:02 +0300

    versin 1

---
+++ index.js
@@ -0,0 +1,37 @@
+const express = require('express')
+const {engine} = require('express-handlebars')
+const app = express()
+require('dotenv').config()
+
+
+
+
+app.use(express.static('public'))
+
+app.engine('handlebars', engine())
+app.set('view engine', 'handlebars')
+app.set('views', './views')
+
+
+
+app.get('/', (req, res)=>{
+    res.render('site/index')
+})
+app.get('*', (req, res)=>{
+    res.render('error/404')
+})
+
+
+
+
+
+
+
+
+
+
+
+
+app.listen(process.env.PORT, ()=>{
+    console.log(`http://127.0.0.1:${process.env.PORT}/`)
+})
